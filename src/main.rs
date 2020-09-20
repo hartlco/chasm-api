87a324437fd4457e6353b9da8368a74c68c13fc9
Martin Hartl <martin@hartl.co>
2020-09-20 07:03:17 +0200

    Add support for pushing and committing files with basic content parts

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
+use actix_web::client::Client;
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
 use json::JsonValue;
 
-use reqwest::header::AUTHORIZATION;
-use reqwest::header::CONTENT_TYPE;
-use reqwest::header::USER_AGENT;
+use chrono::{DateTime, Utc};
 
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
 use serde_json::{Result, Value};
+
+use base64::encode;
 
 #[derive(Deserialize, Serialize)]
 struct CommitContent {
@@ -17,44 +18,108 @@ struct CommitContent {
     path: String,
 }
 
+impl CommitContent {
+    fn new(message: String, content: String, path: String) -> CommitContent {
+        CommitContent {
+            message: message,
+            content: encode(content),
+            path: path,
+        }
+    }
+
+    fn new_from_image(message: String, content: Vec<u8>, path: String) -> CommitContent {
+        CommitContent {
+            message: message,
+            content: encode(content),
+            path: path,
+        }
+    }
+}
+
 #[derive(Deserialize, Serialize)]
 struct PostContent {
     repo: String,
     access_token: String,
-    content: Vec<ContentPart>
+    title: Option<String>,
+    content: Vec<ContentPart>,
 }
 
 #[derive(Deserialize, Serialize)]
 enum ContentPart {
-    Title(String),
-    Paragraph(String)
+    Header { text: String },
+    Paragraph { text: String },
+    Image { link: String },
 }
 
-fn commit(repo: String, access_token: String, content: CommitContent) {
+async fn commit(repo: String, access_token: String, content: CommitContent) {
     let repository = repo;
 
-  let post_url = format!(
-        "https://api.github.com/repos/{}/contents/{}", repository, 
-        content.path
+    let post_url = format!(
+        "https://api.github.com/repos/{}/contents/{}",
+        repository, content.path
     );
     let token = access_token;
-    let new_put = reqwest::blocking::Client::new()
-        .put(&post_url)
-        .header(USER_AGENT, "pan-rust")
-        .header(AUTHORIZATION, format!("Bearer {}", token))
-        .header(CONTENT_TYPE, "application/json;charset=UTF-8")
-        .body(serde_json::to_string(&content).unwrap())
-        .send()
-        .unwrap();
-        
 
-    // let commit_response: CommitResponse = new_put.json().unwrap();
-    // return commit_response
+    let client = Client::default();
+
+    let response = client
+        .put(post_url)
+        .header("User-Agent", "actix-web/3.0")
+        .header("Authorization", format!("Bearer {}", token))
+        .header("Content-Type", "application/json;charset=UTF-8")
+        .send_body(serde_json::to_string(&content).unwrap())
+        .await;
+
+    println!("Response: {:?}", response);
 }
 
 async fn post_content(content: web::Json<PostContent>) -> HttpResponse {
     println!("model: {:?}", &content.access_token);
-    HttpResponse::Ok().json(content.0) // <- send response
+
+    let now: DateTime<Utc> = Utc::now();
+    let filename_date = now.format("%Y-%m-%dT%H:%M:%SZ");
+    let date = now.format("%Y-%m-%dT%H:%M:%SZ");
+
+    let mut body_string = "".to_string();
+
+    let mut title_string = "".to_string();
+
+    if let Some(title) = &content.title {
+        title_string.push_str(&title);
+    }
+
+    for content_part in &content.content {
+        match content_part {
+            ContentPart::Header { text } => {
+                let header_string = format!("## {}\n", text);
+                body_string.push_str(&header_string);
+            }
+            ContentPart::Paragraph { text } => {
+                let paragraph_string = format!("{}\n", text);
+                body_string.push_str(&paragraph_string);
+            }
+            ContentPart::Image { link } => {
+                let image_string = format!("![]({})\n", link);
+                body_string.push_str(&image_string);
+            }
+        }
+        // body_string.push_str(&image_string);
+    }
+
+    let content_string = format!("+++\ntitle = \"{}\"\ndate = {}\n+++\n{}\n\n<!-- more -->", title_string, date, body_string);
+
+    let c2 = CommitContent::new(
+        "Add post".to_string(),
+        content_string,
+        format!("content/{}.md", filename_date),
+    );
+
+    commit(
+        content.repo.to_string(),
+        content.access_token.to_string(),
+        c2,
+    ).await;
+    HttpResponse::Ok().json(content.0)
 }
 
 #[get("/")]
